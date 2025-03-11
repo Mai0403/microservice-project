d5dfca9d0ee1d15cfeb54d0f5fbd0f5e6424209a
mai <sreymai.phou@gmail.com>
2025-03-11 13:17:38 +0700

    modified auth server.js

--- auth-service/server.js
+++ auth-service/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
+dotenv.config({ path: './auth-service/.env' });
 connectDB();
 
 const app = express();
@@ -13,4 +14,9 @@ app.use(cors());
 
 app.use("/auth", authRoutes);
 
-app.listen(5002, () => console.log("Auth Service running on port 5002"));
+// app.listen(5002, () => console.log("Auth Service running on port 5002"));
+
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, () => {
+  console.log(`Auth Service running on port ${PORT}`);
+});
